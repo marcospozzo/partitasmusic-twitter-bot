48644155a8133bf173e6076639abbd46385a6b82
Marc Pozzo <pozzomarc@gmail.com>
2023-03-07 03:45:42 -0300

    commenting the discord part

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const sendAphorism = async () => {
     const discordChannel = discordClient.channels.cache.find(
       (channel) => channel.name === "📰daily-gc-aphorism"
     );
-    await discordChannel.send(`*${aphorismWithQuotes}*`); // * makes it italics
+    // await discordChannel.send(`*${aphorismWithQuotes}*`); // * makes it italics
 
     // Twitter
     await twitterClient.post("statuses/update", {
