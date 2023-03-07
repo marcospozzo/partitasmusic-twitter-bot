87b3338f806b98f04ba34ecf0c108b99b0143507
Marc Pozzo <pozzomarc@gmail.com>
2023-03-07 03:06:38 -0300

    test

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const Twit = require("twit");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const discordClient = new Discord.Client();
-discordClient.once("ready", () => {
-  console.log("Discord ready!");
-});
-
-discordClient.login(process.env.DISCORD_TOKEN);
-
-const twitterClient = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
-
 const sendAphorism = async () => {
+  const discordClient = new Discord.Client();
+  discordClient.once("ready", () => {
+    console.log("Discord ready!");
+  });
+
+  discordClient.login(process.env.DISCORD_TOKEN);
+
+  const twitterClient = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
   try {
     const response = await fetch("https://partitasmusic.com/api/aphorism");
     const aphorism = await response.text();
